36e5e77c737311938bc9cbda625ead7a1cd34a97
Sune Debel <1228354+suned@users.noreply.github.com>
2025-06-20 23:16:58 +0200

    chore: remove unused dependencies

--- flake.nix
+++ flake.nix
@@ -4,18 +4,17 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { flake-utils, nixpkgs, home-manager, nix-vscode-extensions, ... }: flake-utils.lib.eachDefaultSystem
+  outputs = { flake-utils, nixpkgs, home-manager, ... }: flake-utils.lib.eachDefaultSystem
     (system:
       {
         formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-        mkConfiguration = { user, ssh_config ? [ ] }: import ./mkConfiguration.nix { inherit user ssh_config nixpkgs nix-vscode-extensions home-manager system; };
+        mkConfiguration = { user, ssh_config ? [ ] }: import ./mkConfiguration.nix { inherit user ssh_config nixpkgs home-manager system; };
       }
     ) // {
     templates.default = {
--- mkConfiguration.nix
+++ mkConfiguration.nix
@@ -1,7 +1,6 @@
-{ user, ssh_config, nixpkgs, home-manager, nix-vscode-extensions, system, ... }:
+{ user, ssh_config, nixpkgs, home-manager, system, ... }:
 let
   pkgs = nixpkgs.legacyPackages.${system};
-  community-extensions = nix-vscode-extensions.extensions.${system};
   home = { homeDirectory = "/Users/${user.username}"; username = user.username; };
 in
 home-manager.lib.homeManagerConfiguration {
@@ -9,7 +8,6 @@ home-manager.lib.homeManagerConfiguration {
 
   modules = [ ./home.nix { inherit home; } ];
   extraSpecialArgs = {
-    vscode-marketplace = community-extensions.vscode-marketplace;
     inherit user ssh_config;
   };
 }
